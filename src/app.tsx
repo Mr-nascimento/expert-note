63a74c8df4d0645adfac478c2a8bf3ee1ba9fae4
Igor Nascimento <igortjnascimento@gmail.com>
2024-02-08 20:20:55 -0300

    add  dialogs

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,11 @@
 import logo from "./assets/logo-expert-voice.svg";
+import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
+
+const note = {
+  date: new Date(),
+  content: 'Hello'
+}
 
 export function App() {
   return (
@@ -14,18 +20,8 @@ export function App() {
       </form>
       <div className="h-px bg-slate-400" />
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <div className="rounded-md bg-slate-700 p-5 space-y-3">
-          <span className="text-sm font-medium text-slate-200">
-            Adicionar nota
-          </span>
-          <p className="text-sm leading-6 text-slate-400">
-            Grave uma nota em áudio que será convertida para texto
-            automaticamente.
-          </p>
-        </div>
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NewNoteCard />        
+        <NoteCard note={note}/>
       </div>
     </div>
   );
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,14 +1,48 @@
-export function NoteCard() {
+import * as Dialog from "@radix-ui/react-dialog";
+import { formatDistanceToNow} from 'date-fns'
+import {ptBR} from 'date-fns/locale'
+import {X} from 'lucide-react'
+
+interface NoteCardProps {
+  note: {
+    date: Date
+    content: string
+  }
+}
+
+export function NoteCard({ note }: NoteCardProps) {
   return (
-    <button className="text-left hover:ring-2 hover:ring-slate-600 rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none focus-visible:ring-2 focus-visible:ring-lime-400">
-      <span className="text-sm font-medium text-slate-300">há 2 dias</span>
-      <p className="text-sm leading-6 text-slate-400">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
-        temporibus exercitationem tempora saepe sapiente ut laudantium.
-        Architecto ducimus quo, corrupti laboriosam quam assumenda ipsum, magnam
-        labore adipisci minima voluptate dolores.
-      </p>
-      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/40 to-black/0 pointer-events-none" />
-    </button>
+    <Dialog.Root>
+      <Dialog.Trigger className="text-left rounded-md bg-slate-800 p-5 flex flex-col gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+        <span className="text-sm font-medium text-slate-300">
+          {formatDistanceToNow(note.date, {locale: ptBR, addSuffix: true})}
+        </span>
+        <p className="text-sm leading-6 text-slate-400">
+          {note.content}
+        </p>
+        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/40 to-black/0 pointer-events-none" />
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="inset-0 fixed bg-black/50">
+          <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+            <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+              <X className="size-5"/>              
+            </Dialog.Close>
+            <div className="flex flex-1 flex-col gap-3 p-5">
+              <span className="text-sm font-medium text-slate-300">
+                {formatDistanceToNow(note.date, {locale: ptBR, addSuffix: true})}
+              </span>
+              <p className="text-sm leading-6 text-slate-400">
+                {note.content}
+              </p>
+            </div>
+
+            <button type="button" className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group">
+              Deseja <span className="text-red-400 group-hover:underline">apagar essa nota?</span> 
+            </button>
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 }
